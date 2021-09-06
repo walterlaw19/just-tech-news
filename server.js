7af42c81bb8992f75d64d118f53a26a4292e14c1
SC2021 <walterlaw19@gmail.com>
2021-09-06 18:13:49 -0400

    add model Posts

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // turn on routes
 app.use(routes);
 
-// turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// turn on connection to database and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
+
